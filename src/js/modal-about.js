060603a86e29d92d9631a9ba740073a08a30d007
Marina Boikova <boymar84@gmail.com>
2021-10-21 18:16:50 +0300

    добавляет модальное окно на секцию "About"

---
+++ src/js/modal-about.js
@@ -0,0 +1,12 @@
+const refs = {
+    openModalBtn: document.querySelector("[data-open-about]"),
+    closeModalBtn: document.querySelector("[data-close-about]"),
+    backdrop: document.querySelector("[data-backdrop-about]"),
+  };
+  
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+  function toggleModal() {
+    refs.backdrop.classList.toggle("visually-hidden");
+  }
