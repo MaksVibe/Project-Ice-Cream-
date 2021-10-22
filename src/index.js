bd436d5bd02d6830e63cf59934df2918b27b08f8
MaksVibe <parunovmaksym@gmail.com>
2021-10-22 04:00:13 +0300

    Bug-fix modal windows

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import './sass/main.scss';
+import "./sass/main.scss";
 
-import './js/contacts-modal.js';
-import './js/map.js';
+import "./js/contacts-modal.js";
+import "./js/map.js";
+import "./js/modal-about.js";
+import "./js/mobile-menu.js";
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,37 +1,40 @@
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-  const menuList = document.querySelector('.mobile-menu__list');
-  const headerBtn = document.querySelector('.header__button');
+  const mobileMenu = document.querySelector(".js-menu-container");
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  const closeMenuBtn = document.querySelector(".js-close-menu");
+  const menuList = document.querySelector(".mobile-menu__list");
+  const headerBtn = document.querySelector(".header__button");
 
   const toggleMenu = () => {
-    headerBtn.classList.toggle('is-hidden');
-    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+    headerBtn.classList.toggle("is-hidden");
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
 
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+    const scrollLockMethod = !isMenuOpen
+      ? "disableBodyScroll"
+      : "enableBodyScroll";
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
   const removeMenu = () => {
-    headerBtn.classList.remove('is-hidden');
-    mobileMenu.classList.remove('is-open');
-    const scrollLockMethod = 'enableBodyScroll';
+    headerBtn.classList.remove("is-hidden");
+    mobileMenu.classList.remove("is-open");
+    const scrollLockMethod = "enableBodyScroll";
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-  menuList.addEventListener('click', removeMenu);
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+  menuList.addEventListener("click", removeMenu);
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
